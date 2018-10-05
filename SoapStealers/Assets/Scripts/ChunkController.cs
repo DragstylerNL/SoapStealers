26b2df6a9a13731d603ba5b34b1935afb5a6d3dc
DragstylerNL <dragstylernl@gmail.com>
2018-10-05 11:26:30 +0200

    push

---
+++ SoapStealers/Assets/Scripts/ChunkController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkController : MonoBehaviour {
+
+    private List<GameObject> chunksLst = new List<GameObject>();
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
