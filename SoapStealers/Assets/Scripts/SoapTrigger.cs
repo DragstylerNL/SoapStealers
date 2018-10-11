3ebeb94dcf3d19a9c60bb43ccedf188a20f829c0
DragstylerNL <dragstylernl@gmail.com>
2018-10-11 09:43:47 +0200

    random shit complete

---
+++ SoapStealers/Assets/Scripts/SoapTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoapTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject.FindGameObjectWithTag("ChunkController").GetComponent<ChunkController>().SpawnNextChunk();
+        Destroy(transform.parent.gameObject);
+    }
+}
