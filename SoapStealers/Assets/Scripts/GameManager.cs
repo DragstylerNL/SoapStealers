6078b9a2dce500fc3e065e6a5719ac96d95a497a
DragstylerNL <dragstylernl@gmail.com>
2018-10-12 14:30:36 +0200

    shit

---
+++ SoapStealers/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Die()
+    {
+        GameObject.FindGameObjectWithTag("Death").transform.position = new Vector3(0, 0, 0);
+    }
+}
